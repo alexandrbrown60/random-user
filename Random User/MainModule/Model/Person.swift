1e5f259abae175e11a530899dcef3d419c1426ef
Alexander Ivanov <alexandrbrown60@gmail.com>
2022-03-20 16:38:23 +0300

    person model added

---
+++ Random User/MainModule/Model/Person.swift
@@ -0,0 +1,43 @@
+//
+//  Person.swift
+//  Random User
+//
+//  Created by Александр Иванов on 20.03.2022.
+//
+
+import Foundation
+
+struct Result: Decodable {
+    let result: [Person]
+}
+
+struct Person: Decodable {
+    let gender: String
+    let name: PersonName
+    let location: Location
+    let email: String
+    let phone: String
+    let picture: Picture
+}
+
+struct PersonName: Decodable {
+    let first: String
+    let last: String
+}
+
+struct Location: Decodable {
+    let street: Street
+    let city: String
+    let country: String
+}
+
+struct Street: Decodable {
+    let number: Int
+    let name: String
+}
+
+struct Picture: Decodable {
+    let large: String
+}
+
+
