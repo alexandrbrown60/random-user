29721cb34d79defed0c6cd1c5a870e1561ea8465
Alexander Ivanov <alexandrbrown60@gmail.com>
2022-03-20 16:20:53 +0300

    start project

---
+++ Random User/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random User
+//
+//  Created by Александр Иванов on 20.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
